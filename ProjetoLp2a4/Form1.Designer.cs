28d8676168eafda9b5dc06d24ebeb73bbb82399a
Thule <luizaeana5818@hotmail.com>
2023-04-06 22:01:32 -0300

    Tela inicial finalizada

---
+++ ProjetoLp2a4/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProjetoLp2a4
+{
+    partial class PaginaPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labemail = new System.Windows.Forms.Label();
+            this.labelsenha = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.botlogin = new System.Windows.Forms.Button();
+            this.linkcadastro = new System.Windows.Forms.LinkLabel();
+            this.linkredefinir = new System.Windows.Forms.LinkLabel();
+            this.checlembrar = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labemail
+            // 
+            this.labemail.AutoSize = true;
+            this.labemail.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labemail.ForeColor = System.Drawing.Color.White;
+            this.labemail.Location = new System.Drawing.Point(575, 175);
+            this.labemail.Name = "labemail";
+            this.labemail.Size = new System.Drawing.Size(87, 27);
+            this.labemail.TabIndex = 0;
+            this.labemail.Text = "E-MAIL";
+            // 
+            // labelsenha
+            // 
+            this.labelsenha.AutoSize = true;
+            this.labelsenha.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelsenha.ForeColor = System.Drawing.Color.White;
+            this.labelsenha.Location = new System.Drawing.Point(575, 251);
+            this.labelsenha.Name = "labelsenha";
+            this.labelsenha.Size = new System.Drawing.Size(81, 27);
+            this.labelsenha.TabIndex = 1;
+            this.labelsenha.Text = "SENHA";
+            // 
+            // txtemail
+            // 
+            this.txtemail.BackColor = System.Drawing.Color.Honeydew;
+            this.txtemail.Location = new System.Drawing.Point(580, 216);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(307, 23);
+            this.txtemail.TabIndex = 2;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.BackColor = System.Drawing.Color.Honeydew;
+            this.txtsenha.Location = new System.Drawing.Point(580, 293);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.PasswordChar = '*';
+            this.txtsenha.Size = new System.Drawing.Size(307, 23);
+            this.txtsenha.TabIndex = 3;
+            // 
+            // botlogin
+            // 
+            this.botlogin.BackColor = System.Drawing.Color.OliveDrab;
+            this.botlogin.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botlogin.ForeColor = System.Drawing.Color.White;
+            this.botlogin.Location = new System.Drawing.Point(580, 408);
+            this.botlogin.Name = "botlogin";
+            this.botlogin.Size = new System.Drawing.Size(307, 36);
+            this.botlogin.TabIndex = 4;
+            this.botlogin.Text = "LOGIN";
+            this.botlogin.UseVisualStyleBackColor = false;
+            // 
+            // linkcadastro
+            // 
+            this.linkcadastro.AutoSize = true;
+            this.linkcadastro.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkcadastro.LinkColor = System.Drawing.Color.Honeydew;
+            this.linkcadastro.Location = new System.Drawing.Point(576, 366);
+            this.linkcadastro.Name = "linkcadastro";
+            this.linkcadastro.Size = new System.Drawing.Size(104, 22);
+            this.linkcadastro.TabIndex = 5;
+            this.linkcadastro.TabStop = true;
+            this.linkcadastro.Text = "Cadastrar-se";
+            // 
+            // linkredefinir
+            // 
+            this.linkredefinir.AutoSize = true;
+            this.linkredefinir.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkredefinir.LinkColor = System.Drawing.Color.Honeydew;
+            this.linkredefinir.Location = new System.Drawing.Point(728, 328);
+            this.linkredefinir.Name = "linkredefinir";
+            this.linkredefinir.Size = new System.Drawing.Size(159, 22);
+            this.linkredefinir.TabIndex = 6;
+            this.linkredefinir.TabStop = true;
+            this.linkredefinir.Text = "Esqueceu sua senha?";
+            // 
+            // checlembrar
+            // 
+            this.checlembrar.AutoSize = true;
+            this.checlembrar.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checlembrar.ForeColor = System.Drawing.Color.Honeydew;
+            this.checlembrar.Location = new System.Drawing.Point(580, 327);
+            this.checlembrar.Name = "checlembrar";
+            this.checlembrar.Size = new System.Drawing.Size(110, 26);
+            this.checlembrar.TabIndex = 7;
+            this.checlembrar.Text = "Lembre-me";
+            this.checlembrar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProjetoLp2a4.Properties.Resources.people;
+            this.pictureBox2.Location = new System.Drawing.Point(671, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(116, 108);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoLp2a4.Properties.Resources.MONTANHAVERDE1;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 526);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PaginaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checlembrar);
+            this.Controls.Add(this.linkredefinir);
+            this.Controls.Add(this.linkcadastro);
+            this.Controls.Add(this.botlogin);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.labelsenha);
+            this.Controls.Add(this.labemail);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "PaginaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MaSky";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labemail;
+        private System.Windows.Forms.Label labelsenha;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Button botlogin;
+        private System.Windows.Forms.LinkLabel linkcadastro;
+        private System.Windows.Forms.LinkLabel linkredefinir;
+        private System.Windows.Forms.CheckBox checlembrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
